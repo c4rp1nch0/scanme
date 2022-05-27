f44137662ea2d4c8acf754f2454b860c9244dc30
c4rp1nch0 <c4rp1nch0@protonmail.com>
2022-05-27 13:07:13 -0300

    test private action

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ var a = {}; a[b]=4;
 var password = "EoCe7JV3HtzKEoCe7JV3HtzK"
 var c = {}; c[b]=2;
 var c = {}; c[b]=2;
+var c = {}; c[b]=2;
+var c = {}; c[b]=2;
+var c = {}; c[b]=2;
+var dwc = {}; c[b]=2;
