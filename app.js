28854ec6d1f887c769a654e90a873b788ad1241e
c4rp1nch0 <c4rp1nch0@protonmail.com>
2022-05-27 15:51:21 -0300

    leak a secret

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var a = {}; a[b]=4;
 
 
 var token = "EoCe7JV3HtzKEoCe7JV3HtzK"
+var secret = "EoCe7JV3HtzKEoCe7JV3HtzK"
 var password = "EoCe7JV3HtzKEoCe7JV3HtzK"
 var c = {}; c[b]=2;
 var c = {}; c[b]=2;
