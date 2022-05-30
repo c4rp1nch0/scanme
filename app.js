17f15c4c5ee56b2672f3a8054480800eed2d8024
c4rp1nch0 <c4rp1nch0@protonmail.com>
2022-05-30 11:56:13 -0300

    add new password

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var a = {}; a[b]=4;
 var token = "EoCe7JV3HtzKEoCe7JV3HtzK"
 var password = "EoCe7JV3HtzKEoCe7JV3HtzK"
 
-var password = "EoCe7JV3HtzKEoCe7JV3HtzK"
+var password = "Ow56Zle9T1GA4gvro1TuqOk8sztQnyoVN6HYUKOVXsj1wOoQz24QqDIab88mDWrUogfGNFaxqP6oU6UmA9rDQKgZ0GPnuD7F"
 //var password = "EoCe7JV3HtzKEoCe7JV3HtzK"
 //var password = "EoCe7JV3HtzKEoCe7JV3HtzK"
 //var password = "EoCe7JV3HtzKEoCe7JV3HtzK"
